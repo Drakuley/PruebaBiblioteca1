ac814b96621269fa3ec0d8fbc15ba2009f27e37c
Drakuley <55518578+Drakuley@users.noreply.github.com>
2019-09-23 21:05:47 -0500

    3
    3

---
+++ Forms/Miguel2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaBiblioteca1.Forms
+{
+    public partial class Miguel2 : Form
+    {
+        public Miguel2()
+        {
+            InitializeComponent();
+        }
+
+        private void Miguel2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
