801cfd14db53c91c3e4a4b4407aad73b65ef21b6
sofiabelmares <55592288+sofiabelmares@users.noreply.github.com>
2019-09-23 21:09:06 -0500

    cambios de sfia2

---
+++ Forms/sofia2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaBiblioteca1.Forms
+{
+    public partial class sofia2 : Form
+    {
+        public sofia2()
+        {
+            InitializeComponent();
+        }
+    }
+}
