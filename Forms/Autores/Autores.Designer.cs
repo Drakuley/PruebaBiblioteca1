6e8dfdceabd633a229980606e92b9a220d424c21
martincgro <martin.acg@hotmail.com>
2019-09-25 19:27:04 -0500

    Formulario de la tabla autores

---
+++ Forms/Autores/Autores.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace PruebaBiblioteca1.Forms.Autores
+{
+    partial class Autores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAutores = new System.Windows.Forms.DataGridView();
+            this.txtAutoresRegistrados = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblIdAutor = new System.Windows.Forms.Label();
+            this.lblNombreAutor = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_Registrar = new System.Windows.Forms.Button();
+            this.IdAutor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreAutor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAutores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAutores
+            // 
+            this.dgvAutores.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvAutores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAutores.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdAutor,
+            this.NombreAutor});
+            this.dgvAutores.Location = new System.Drawing.Point(25, 37);
+            this.dgvAutores.Name = "dgvAutores";
+            this.dgvAutores.Size = new System.Drawing.Size(466, 286);
+            this.dgvAutores.TabIndex = 0;
+            // 
+            // txtAutoresRegistrados
+            // 
+            this.txtAutoresRegistrados.AutoSize = true;
+            this.txtAutoresRegistrados.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAutoresRegistrados.Location = new System.Drawing.Point(188, 9);
+            this.txtAutoresRegistrados.Name = "txtAutoresRegistrados";
+            this.txtAutoresRegistrados.Size = new System.Drawing.Size(150, 16);
+            this.txtAutoresRegistrados.TabIndex = 1;
+            this.txtAutoresRegistrados.Text = "Autores Registrados";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(608, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(608, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lblIdAutor
+            // 
+            this.lblIdAutor.AutoSize = true;
+            this.lblIdAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdAutor.Location = new System.Drawing.Point(507, 49);
+            this.lblIdAutor.Name = "lblIdAutor";
+            this.lblIdAutor.Size = new System.Drawing.Size(61, 16);
+            this.lblIdAutor.TabIndex = 4;
+            this.lblIdAutor.Text = "ID Autor :";
+            // 
+            // lblNombreAutor
+            // 
+            this.lblNombreAutor.AutoSize = true;
+            this.lblNombreAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreAutor.Location = new System.Drawing.Point(507, 120);
+            this.lblNombreAutor.Name = "lblNombreAutor";
+            this.lblNombreAutor.Size = new System.Drawing.Size(63, 16);
+            this.lblNombreAutor.TabIndex = 5;
+            this.lblNombreAutor.Text = "Nombre :";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LemonChiffon;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(530, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 36);
+            this.button2.TabIndex = 36;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // btn_Registrar
+            // 
+            this.btn_Registrar.BackColor = System.Drawing.Color.LemonChiffon;
+            this.btn_Registrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Registrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Registrar.Location = new System.Drawing.Point(530, 233);
+            this.btn_Registrar.Name = "btn_Registrar";
+            this.btn_Registrar.Size = new System.Drawing.Size(178, 36);
+            this.btn_Registrar.TabIndex = 35;
+            this.btn_Registrar.Text = "Aceptar";
+            this.btn_Registrar.UseVisualStyleBackColor = false;
+            // 
+            // IdAutor
+            // 
+            this.IdAutor.HeaderText = "ID Autor";
+            this.IdAutor.Name = "IdAutor";
+            this.IdAutor.ReadOnly = true;
+            // 
+            // NombreAutor
+            // 
+            this.NombreAutor.HeaderText = "Nombre";
+            this.NombreAutor.Name = "NombreAutor";
+            this.NombreAutor.ReadOnly = true;
+            // 
+            // Autores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 335);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_Registrar);
+            this.Controls.Add(this.lblNombreAutor);
+            this.Controls.Add(this.lblIdAutor);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtAutoresRegistrados);
+            this.Controls.Add(this.dgvAutores);
+            this.Name = "Autores";
+            this.Text = "Autores";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAutores)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAutores;
+        private System.Windows.Forms.Label txtAutoresRegistrados;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblIdAutor;
+        private System.Windows.Forms.Label lblNombreAutor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdAutor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreAutor;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_Registrar;
+    }
+}
