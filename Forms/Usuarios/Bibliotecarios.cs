e9931eca0f4548775c6ca74ede057b13eb9093d1
sofiabelmares <55592288+sofiabelmares@users.noreply.github.com>
2019-09-26 21:38:08 -0500

    Forms biblio usuarios

--- Forms/Usuarios/Bibliotecarios.cs
+++ Forms/Usuarios/Bibliotecarios.cs
@@ -12,6 +12,24 @@ namespace PruebaBiblioteca1.Forms
 {
     public partial class Bibliotecarios : Form
     {
+        private DataGridViewTextBoxColumn Turno;
+        private Label label6;
+        private DataGridViewTextBoxColumn NombreBibliotecario;
+        private DataGridViewTextBoxColumn idBibliotecario;
+        private TextBox txtIdBibliotecario;
+        private Label label5;
+        private ComboBox cbTurno;
+        private Label label4;
+        private TextBox txtContraseña;
+        private Label direccion;
+        private DataGridView dgvBibliotecarios;
+        private DataGridViewTextBoxColumn Contraseña;
+        private Panel panel1;
+        private Button btnEliminar;
+        private Button btnGuardar;
+        private Label Nombre;
+        private TextBox txtNombreBibliotecario;
+
         public Bibliotecarios()
         {
             InitializeComponent();
@@ -19,13 +37,212 @@ namespace PruebaBiblioteca1.Forms
 
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Turno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label6 = new System.Windows.Forms.Label();
+            this.NombreBibliotecario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idBibliotecario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtIdBibliotecario = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbTurno = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.direccion = new System.Windows.Forms.Label();
+            this.dgvBibliotecarios = new System.Windows.Forms.DataGridView();
+            this.Contraseña = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.txtNombreBibliotecario = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBibliotecarios)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Turno
+            // 
+            this.Turno.HeaderText = "Turno";
+            this.Turno.Name = "Turno";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(237, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 24);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "Bibliotecarios";
+            // 
+            // NombreBibliotecario
+            // 
+            this.NombreBibliotecario.HeaderText = "Nombre";
+            this.NombreBibliotecario.Name = "NombreBibliotecario";
+            // 
+            // idBibliotecario
+            // 
+            this.idBibliotecario.HeaderText = "ID Bibliotecario";
+            this.idBibliotecario.Name = "idBibliotecario";
+            // 
+            // txtIdBibliotecario
+            // 
+            this.txtIdBibliotecario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdBibliotecario.Location = new System.Drawing.Point(721, 40);
+            this.txtIdBibliotecario.Name = "txtIdBibliotecario";
+            this.txtIdBibliotecario.Size = new System.Drawing.Size(151, 22);
+            this.txtIdBibliotecario.TabIndex = 39;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(640, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 16);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "ID:";
+            // 
+            // cbTurno
+            // 
+            this.cbTurno.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTurno.FormattingEnabled = true;
+            this.cbTurno.Location = new System.Drawing.Point(721, 120);
+            this.cbTurno.Name = "cbTurno";
+            this.cbTurno.Size = new System.Drawing.Size(151, 24);
+            this.cbTurno.TabIndex = 37;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(640, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "Turno:";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseña.Location = new System.Drawing.Point(721, 92);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(151, 22);
+            this.txtContraseña.TabIndex = 35;
+            // 
+            // direccion
+            // 
+            this.direccion.AutoSize = true;
+            this.direccion.BackColor = System.Drawing.Color.RoyalBlue;
+            this.direccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.direccion.ForeColor = System.Drawing.Color.White;
+            this.direccion.Location = new System.Drawing.Point(640, 97);
+            this.direccion.Name = "direccion";
+            this.direccion.Size = new System.Drawing.Size(80, 16);
+            this.direccion.TabIndex = 34;
+            this.direccion.Text = "Contraseña:";
+            // 
+            // dgvBibliotecarios
+            // 
+            this.dgvBibliotecarios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvBibliotecarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBibliotecarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idBibliotecario,
+            this.NombreBibliotecario,
+            this.Contraseña,
+            this.Turno});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvBibliotecarios.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvBibliotecarios.Location = new System.Drawing.Point(6, 40);
+            this.dgvBibliotecarios.Name = "dgvBibliotecarios";
+            this.dgvBibliotecarios.Size = new System.Drawing.Size(615, 410);
+            this.dgvBibliotecarios.TabIndex = 33;
+            // 
+            // Contraseña
+            // 
+            this.Contraseña.HeaderText = "Contraseña";
+            this.Contraseña.Name = "Contraseña";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel1.Controls.Add(this.btnEliminar);
+            this.panel1.Controls.Add(this.btnGuardar);
+            this.panel1.Location = new System.Drawing.Point(633, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(251, 465);
+            this.panel1.TabIndex = 41;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(163, 165);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 28;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(87, 165);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 27;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nombre.ForeColor = System.Drawing.Color.White;
+            this.Nombre.Location = new System.Drawing.Point(640, 69);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(60, 16);
+            this.Nombre.TabIndex = 32;
+            this.Nombre.Text = "Nombre:";
+            // 
+            // txtNombreBibliotecario
+            // 
+            this.txtNombreBibliotecario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreBibliotecario.Location = new System.Drawing.Point(721, 66);
+            this.txtNombreBibliotecario.Name = "txtNombreBibliotecario";
+            this.txtNombreBibliotecario.Size = new System.Drawing.Size(151, 22);
+            this.txtNombreBibliotecario.TabIndex = 31;
             // 
             // Bibliotecarios
             // 
-            this.ClientSize = new System.Drawing.Size(592, 384);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtIdBibliotecario);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbTurno);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.direccion);
+            this.Controls.Add(this.dgvBibliotecarios);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.txtNombreBibliotecario);
+            this.Controls.Add(this.panel1);
             this.Name = "Bibliotecarios";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBibliotecarios)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
     }
--- Forms/Usuarios/Usuarios.cs
+++ Forms/Usuarios/Usuarios.cs
@@ -12,6 +12,28 @@ namespace PruebaBiblioteca1.Forms
 {
     public partial class Usuarios : Form
     {
+        private Button btnGenerarReporteUsuario;
+        private Label label6;
+        private Button btnEliminar;
+        private Button btnGuardar;
+        private TextBox txtIdUsuario;
+        private Label label5;
+        private ComboBox cbGeneroUsuario;
+        private Label label4;
+        private Label label3;
+        private TextBox txtTelefono;
+        private TextBox txtDireccion;
+        private Label direccion;
+        private DataGridView dgvUsuarios;
+        private DataGridViewTextBoxColumn IDUsuario;
+        private DataGridViewTextBoxColumn NombreUsuario;
+        private DataGridViewTextBoxColumn DireccionUsuario;
+        private DataGridViewTextBoxColumn TelefonoUsuario;
+        private DataGridViewTextBoxColumn GeneroUsuario;
+        private Label Nombre;
+        private TextBox txtNombreUsuario;
+        private Panel panel1;
+
         public Usuarios()
         {
             InitializeComponent();
@@ -19,13 +41,244 @@ namespace PruebaBiblioteca1.Forms
 
         private void InitializeComponent()
         {
+            this.btnGenerarReporteUsuario = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtIdUsuario = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbGeneroUsuario = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.direccion = new System.Windows.Forms.Label();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            this.IDUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DireccionUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TelefonoUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GeneroUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.txtNombreUsuario = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnGenerarReporteUsuario
+            // 
+            this.btnGenerarReporteUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarReporteUsuario.Location = new System.Drawing.Point(705, 212);
+            this.btnGenerarReporteUsuario.Name = "btnGenerarReporteUsuario";
+            this.btnGenerarReporteUsuario.Size = new System.Drawing.Size(156, 23);
+            this.btnGenerarReporteUsuario.TabIndex = 46;
+            this.btnGenerarReporteUsuario.Text = "Generar Reporte";
+            this.btnGenerarReporteUsuario.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(261, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 24);
+            this.label6.TabIndex = 45;
+            this.label6.Text = "Usuarios";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(787, 183);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 44;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(706, 183);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 43;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // txtIdUsuario
+            // 
+            this.txtIdUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdUsuario.Location = new System.Drawing.Point(701, 39);
+            this.txtIdUsuario.Name = "txtIdUsuario";
+            this.txtIdUsuario.Size = new System.Drawing.Size(171, 22);
+            this.txtIdUsuario.TabIndex = 42;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(635, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 16);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "ID:";
+            // 
+            // cbGeneroUsuario
+            // 
+            this.cbGeneroUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbGeneroUsuario.FormattingEnabled = true;
+            this.cbGeneroUsuario.Location = new System.Drawing.Point(701, 143);
+            this.cbGeneroUsuario.Name = "cbGeneroUsuario";
+            this.cbGeneroUsuario.Size = new System.Drawing.Size(171, 24);
+            this.cbGeneroUsuario.TabIndex = 40;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(635, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Género:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(635, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 38;
+            this.label3.Text = "Teléfono:";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefono.Location = new System.Drawing.Point(701, 117);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(171, 22);
+            this.txtTelefono.TabIndex = 37;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDireccion.Location = new System.Drawing.Point(701, 91);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(171, 22);
+            this.txtDireccion.TabIndex = 36;
+            // 
+            // direccion
+            // 
+            this.direccion.AutoSize = true;
+            this.direccion.BackColor = System.Drawing.Color.RoyalBlue;
+            this.direccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.direccion.ForeColor = System.Drawing.Color.White;
+            this.direccion.Location = new System.Drawing.Point(635, 92);
+            this.direccion.Name = "direccion";
+            this.direccion.Size = new System.Drawing.Size(68, 16);
+            this.direccion.TabIndex = 35;
+            this.direccion.Text = "Dirección:";
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IDUsuario,
+            this.NombreUsuario,
+            this.DireccionUsuario,
+            this.TelefonoUsuario,
+            this.GeneroUsuario});
+            this.dgvUsuarios.Location = new System.Drawing.Point(11, 39);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.Size = new System.Drawing.Size(606, 410);
+            this.dgvUsuarios.TabIndex = 34;
+            // 
+            // IDUsuario
+            // 
+            this.IDUsuario.HeaderText = "ID Usuario";
+            this.IDUsuario.Name = "IDUsuario";
+            // 
+            // NombreUsuario
+            // 
+            this.NombreUsuario.HeaderText = "Nombre Usuario";
+            this.NombreUsuario.Name = "NombreUsuario";
+            // 
+            // DireccionUsuario
+            // 
+            this.DireccionUsuario.HeaderText = "Dirección";
+            this.DireccionUsuario.Name = "DireccionUsuario";
+            // 
+            // TelefonoUsuario
+            // 
+            this.TelefonoUsuario.HeaderText = "Teléfono";
+            this.TelefonoUsuario.Name = "TelefonoUsuario";
+            // 
+            // GeneroUsuario
+            // 
+            this.GeneroUsuario.HeaderText = "Género";
+            this.GeneroUsuario.Name = "GeneroUsuario";
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nombre.ForeColor = System.Drawing.Color.White;
+            this.Nombre.Location = new System.Drawing.Point(635, 68);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(60, 16);
+            this.Nombre.TabIndex = 33;
+            this.Nombre.Text = "Nombre:";
+            // 
+            // txtNombreUsuario
+            // 
+            this.txtNombreUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreUsuario.Location = new System.Drawing.Point(701, 65);
+            this.txtNombreUsuario.Name = "txtNombreUsuario";
+            this.txtNombreUsuario.Size = new System.Drawing.Size(171, 22);
+            this.txtNombreUsuario.TabIndex = 32;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel1.Location = new System.Drawing.Point(629, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 465);
+            this.panel1.TabIndex = 47;
             // 
             // Usuarios
             // 
-            this.ClientSize = new System.Drawing.Size(525, 373);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.btnGenerarReporteUsuario);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtIdUsuario);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbGeneroUsuario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.direccion);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.txtNombreUsuario);
+            this.Controls.Add(this.panel1);
             this.Name = "Usuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
     }
