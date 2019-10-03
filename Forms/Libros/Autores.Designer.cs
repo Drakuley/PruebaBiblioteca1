cf5f829f37f201f0a9d667d975d9832249e1b402
martincgro <martin.acg@hotmail.com>
2019-10-03 09:42:30 -0500

    Modificacion de interfaz de autores (No Oficial)

--- Forms/Libros/Autores.Designer.cs
+++ Forms/Libros/Autores.Designer.cs
@@ -38,7 +38,10 @@
             this.lblNombreAutor = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.btn_Registrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAutores)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvAutores
@@ -48,9 +51,9 @@
             this.dgvAutores.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.IdAutor,
             this.NombreAutor});
-            this.dgvAutores.Location = new System.Drawing.Point(25, 37);
+            this.dgvAutores.Location = new System.Drawing.Point(12, 37);
             this.dgvAutores.Name = "dgvAutores";
-            this.dgvAutores.Size = new System.Drawing.Size(466, 286);
+            this.dgvAutores.Size = new System.Drawing.Size(467, 286);
             this.dgvAutores.TabIndex = 0;
             // 
             // IdAutor
@@ -78,7 +81,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(575, 49);
+            this.textBox1.Location = new System.Drawing.Point(81, 48);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(186, 22);
             this.textBox1.TabIndex = 2;
@@ -86,7 +89,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(575, 77);
+            this.textBox2.Location = new System.Drawing.Point(81, 76);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(186, 22);
             this.textBox2.TabIndex = 3;
@@ -95,7 +98,7 @@
             // 
             this.lblIdAutor.AutoSize = true;
             this.lblIdAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIdAutor.Location = new System.Drawing.Point(506, 55);
+            this.lblIdAutor.Location = new System.Drawing.Point(12, 54);
             this.lblIdAutor.Name = "lblIdAutor";
             this.lblIdAutor.Size = new System.Drawing.Size(27, 16);
             this.lblIdAutor.TabIndex = 4;
@@ -105,7 +108,7 @@
             // 
             this.lblNombreAutor.AutoSize = true;
             this.lblNombreAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombreAutor.Location = new System.Drawing.Point(506, 83);
+            this.lblNombreAutor.Location = new System.Drawing.Point(12, 82);
             this.lblNombreAutor.Name = "lblNombreAutor";
             this.lblNombreAutor.Size = new System.Drawing.Size(63, 16);
             this.lblNombreAutor.TabIndex = 5;
@@ -114,8 +117,9 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(509, 272);
+            this.button2.Location = new System.Drawing.Point(15, 271);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(252, 51);
             this.button2.TabIndex = 36;
@@ -125,30 +129,51 @@
             // btn_Registrar
             // 
             this.btn_Registrar.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_Registrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Registrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Registrar.Location = new System.Drawing.Point(509, 215);
+            this.btn_Registrar.Location = new System.Drawing.Point(15, 214);
             this.btn_Registrar.Name = "btn_Registrar";
             this.btn_Registrar.Size = new System.Drawing.Size(252, 51);
             this.btn_Registrar.TabIndex = 35;
             this.btn_Registrar.Text = "Guardar";
             this.btn_Registrar.UseVisualStyleBackColor = false;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(205)))));
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btn_Registrar);
+            this.panel1.Controls.Add(this.lblNombreAutor);
+            this.panel1.Controls.Add(this.lblIdAutor);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(494, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(301, 373);
+            this.panel1.TabIndex = 37;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(55)))), ((int)(((byte)(78)))));
+            this.panel2.Location = new System.Drawing.Point(490, -24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 404);
+            this.panel2.TabIndex = 38;
+            // 
             // Autores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(781, 335);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.btn_Registrar);
-            this.Controls.Add(this.lblNombreAutor);
-            this.Controls.Add(this.lblIdAutor);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.txtAutoresRegistrados);
             this.Controls.Add(this.dgvAutores);
             this.Name = "Autores";
             this.Text = "Autores";
             ((System.ComponentModel.ISupportInitialize)(this.dgvAutores)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,5 +191,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn NombreAutor;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button btn_Registrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
