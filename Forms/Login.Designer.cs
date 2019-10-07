96f0919ee0521206a84bedab051397683ed49f88
Drakuley <55518578+Drakuley@users.noreply.github.com>
2019-10-07 09:57:23 -0500

    Prueba

--- Forms/Login.Designer.cs
+++ Forms/Login.Designer.cs
@@ -80,7 +80,7 @@
             this.lblPrestamos.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblPrestamos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPrestamos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
-            this.lblPrestamos.Location = new System.Drawing.Point(104, 19);
+            this.lblPrestamos.Location = new System.Drawing.Point(104, 9);
             this.lblPrestamos.Name = "lblPrestamos";
             this.lblPrestamos.Size = new System.Drawing.Size(243, 22);
             this.lblPrestamos.TabIndex = 54;
