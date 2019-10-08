21c46469d0850e630a3948859f889db07853b362
Drakuley <55518578+Drakuley@users.noreply.github.com>
2019-10-07 20:35:03 -0500

    Seleccionar libros para devoluciones

---
+++ Forms/Devoluciones/GenerarReporteDevoluciones.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PruebaBiblioteca1.Forms.Devoluciones
+{
+    partial class GenerarReporteDevoluciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerarReporte = new System.Windows.Forms.Button();
+            this.lblPrestamos = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerarReporte
+            // 
+            this.btnGenerarReporte.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerarReporte.AutoSize = true;
+            this.btnGenerarReporte.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(64)))), ((int)(((byte)(66)))));
+            this.btnGenerarReporte.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(64)))), ((int)(((byte)(66)))));
+            this.btnGenerarReporte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerarReporte.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarReporte.ForeColor = System.Drawing.Color.White;
+            this.btnGenerarReporte.Location = new System.Drawing.Point(150, 143);
+            this.btnGenerarReporte.Name = "btnGenerarReporte";
+            this.btnGenerarReporte.Size = new System.Drawing.Size(272, 37);
+            this.btnGenerarReporte.TabIndex = 57;
+            this.btnGenerarReporte.Text = "Generar Reporte";
+            this.btnGenerarReporte.UseVisualStyleBackColor = false;
+            // 
+            // lblPrestamos
+            // 
+            this.lblPrestamos.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblPrestamos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrestamos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.lblPrestamos.Location = new System.Drawing.Point(146, 22);
+            this.lblPrestamos.Name = "lblPrestamos";
+            this.lblPrestamos.Size = new System.Drawing.Size(276, 22);
+            this.lblPrestamos.TabIndex = 51;
+            this.lblPrestamos.Text = "Generar Reporte Devoluciones";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.label9.Location = new System.Drawing.Point(28, 89);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(158, 54);
+            this.label9.TabIndex = 50;
+            this.label9.Text = "Seleccione el libro:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(192, 88);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(202, 21);
+            this.comboBox1.TabIndex = 58;
+            // 
+            // GenerarReporteDevoluciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(578, 192);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnGenerarReporte);
+            this.Controls.Add(this.lblPrestamos);
+            this.Controls.Add(this.label9);
+            this.Name = "GenerarReporteDevoluciones";
+            this.Text = "GenerarReporteDevoluciones";
+            this.Load += new System.EventHandler(this.GenerarReporteDevoluciones_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerarReporte;
+        private System.Windows.Forms.Label lblPrestamos;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ Forms/Devoluciones/GenerarReporteDevoluciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaBiblioteca1.Forms.Devoluciones
+{
+    public partial class GenerarReporteDevoluciones : Form
+    {
+        public GenerarReporteDevoluciones()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerarReporteDevoluciones_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
